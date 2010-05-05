85b1aaca6d866885e9c82b33f9a6ff7ddad275a6
Matan Nassau <matan.nassau@gmail.com>
2010-05-05 19:43:53 -0400

    initial commit

---
+++ main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    string compare_with;
+    getline(cin, compare_with);
+    string line;
+    while( getline(cin, line) ) {
+        if( line != compare_with ) { // FIXME: performance, reading line twice
+            cout << "no" << endl;
+            return 0;
+        }
+    }
+    cout << "yes" << endl;
+    return 0;
+}
