25bcc8682c9a0da958759321edfc1d35105ec670
Matan Nassau <matan.nassau@gmail.com>
2010-05-05 21:35:28 -0400

    removed using namespace std

--- main.cc
+++ main.cc
@@ -3,35 +3,33 @@
 #include <algorithm>
 #include <fstream>
 
-using namespace std;
-
 template <class Ch, class Tr>
-int process(basic_istream<Ch,Tr>& in, basic_ostream<Ch,Tr>& out)
+int process(std::basic_istream<Ch,Tr>& in, std::basic_ostream<Ch,Tr>& out)
 {
-    string compare_with;
-    getline(in, compare_with);
-    string line;
-    while( getline(in, line) ) {
-        if( line != compare_with ) { // FIXME: performance, reading line twice
-            out << "no" << endl;
+    std::string compare_with;
+    std::getline(in, compare_with);
+    std::string line;
+    while( std::getline(in, line) ) {
+        if( line != compare_with ) { // FIXME: performance, already read line
+            out << "no\n";
             return 0;
         }
     }
     if( in.bad() ) {
-        cerr << "Error processing input\n";
+        std::cerr << "Error processing input\n";
         return 1;
     }
-    out << "yes" << endl;
+    out << "yes\n";
     return 0;
 }
 
 int main(int argc, char* argv[])
 {
-    fstream fin, fout;
+    std::fstream fin, fout;
     if( argc > 1 && argv[1] != "-" )
-        fin.open(argv[1], ios::in);
+        fin.open(argv[1], std::ios::in);
     if( argc > 2 && argv[2] != "-" )
-        fout.open(argv[2], ios::out);
-    return process(fin.is_open()  ? fin  : cin,
-                   fout.is_open() ? fout : cout);
+        fout.open(argv[2], std::ios::out);
+    return process(fin.is_open() ? fin : std::cin,
+                   fout.is_open() ? fout : std::cout);
 }
