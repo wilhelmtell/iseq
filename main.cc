889cbd7d9b807f9a81e8add6cae54fbcb5ee471d
Matan Nassau <matan.nassau@gmail.com>
2010-05-05 21:19:53 -0400

    added support for in,out files
    run iseq as
    
     ./iseq inputfile outputfile
    
    input file is optional and so is output file, but if outputfile is
    specified then input file must be specified as well. if input file is
    "-" then input is from stdin; if output file is "-" then output is to
    stdout.
    
    The default, when no files are specified, is to use stdin and stdout.

--- main.cc
+++ main.cc
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+template <class Ch, class Tr>
+int process(basic_istream<Ch,Tr>& in, basic_ostream<Ch,Tr>& out)
 {
     string compare_with;
-    getline(cin, compare_with);
+    getline(in, compare_with);
     string line;
-    while( getline(cin, line) ) {
+    while( getline(in, line) ) {
         if( line != compare_with ) { // FIXME: performance, reading line twice
-            cout << "no" << endl;
+            out << "no" << endl;
             return 0;
         }
     }
-    if( cin.bad() ) {
+    if( in.bad() ) {
         cerr << "Error processing input\n";
         return 1;
     }
-    cout << "yes" << endl;
+    out << "yes" << endl;
     return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    fstream fin, fout;
+    if( argc > 1 && argv[1] != "-" )
+        fin.open(argv[1], ios::in);
+    if( argc > 2 && argv[2] != "-" )
+        fout.open(argv[2], ios::out);
+    return process(fin.is_open()  ? fin  : cin,
+                   fout.is_open() ? fout : cout);
 }
