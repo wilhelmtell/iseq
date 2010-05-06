bded710b50cdff83d5ba3dc7ab1ed6005d4a2b39
Matan Nassau <matan.nassau@gmail.com>
2010-05-06 01:26:49 -0400

    check files opened

--- main.cc
+++ main.cc
@@ -26,10 +26,20 @@ int process(std::basic_istream<Ch,Tr>& in, std::basic_ostream<Ch,Tr>& out)
 int main(int argc, char* argv[])
 {
     std::fstream fin, fout;
-    if( argc > 1 && argv[1] != "-" )
+    if( argc > 1 && argv[1] != "-" ) {
         fin.open(argv[1], std::ios::in);
-    if( argc > 2 && argv[2] != "-" )
+        if( ! fin.is_open() ) {
+            std::cerr << "Error openning input file " << argv[1] << '\n';
+            return 1;
+        }
+    }
+    if( argc > 2 && argv[2] != "-" ) {
         fout.open(argv[2], std::ios::out);
+        if( ! fout.is_open() ) {
+            std::cerr << "Error openning output file " << argv[2] << '\n';
+            return 1;
+        }
+    }
     return process(fin.is_open() ? fin : std::cin,
                    fout.is_open() ? fout : std::cout);
 }
